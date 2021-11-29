cbe7e2440a66a407aa39fc14e14ce60347d079aa
Muhammad Towhiduzzaman <86625161+Muhammad-Towhiduzzaman@users.noreply.github.com>
2021-11-29 23:40:14 +0600

    first commit

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Lets build your dream...</h1>
+        <p className="pl-desc">
+          Its a portfolio page only. you'll find lots more inside. explore my demo projects and get your job done with <menu></menu>
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
